7469ff0ab486d790fbb80627ca6458f42001a087
pouyasadri <pouyapaghandeh78@gmail.com>
2024-04-01 16:37:49 +0200

    Initial Commit

---
+++ PastelOnBoarding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PastelOnBoarding
+//
+//  Created by Pouya Sadri on 01/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PastelOnBoarding/PastelOnBoardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PastelOnBoardingApp.swift
+//  PastelOnBoarding
+//
+//  Created by Pouya Sadri on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PastelOnBoardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
